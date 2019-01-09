991ee376e75c607f4f26df2d9e11665467c61605
Andrew Curtis <curtis.andrewm@gmail.com>
2019-01-08 18:56:18 -0800

    Add draft content to app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var willPlay = prompt('Do ya want to play a guessing game about yours truly?');
+console.log('User wants to play:', + willPlay);
+alert('Ok, you answered ' + willPlay);
+
+/* var willPlay = prompt('Should the guessing game be about me?'); 
+    // If not, too bad. Answer again, and this time answer yes
+    // otherwise my questions are done.
+*/

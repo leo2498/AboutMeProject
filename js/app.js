e98054b89e8978a1f1d97d1b81cf3161b83d95cd
Andrew Curtis <curtis.andrewm@gmail.com>
2019-01-08 19:50:30 -0800

    Update app.js to include first question

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-var willPlay = prompt('Do ya want to play a guessing game about yours truly?');
-console.log('User wants to play:', + willPlay);
-alert('Ok, you answered ' + willPlay);
+var willPlay = prompt('Do ya want to play a guessing game ' +
+'about yours truly? Answer "yes" or "no."' +
+' (The answers "y" or "n" are also acceptable.)' );
+console.log('User wants to play:', willPlay);
+if (willPlay.toLowerCase() === 'yes' || willPlay.toLowerCase() === 'y') {
+    alert('Ok, let\'s play!');
+} else if {willPlay.toLowerCase() === 'no' || willPlay.toLowerCase() === 'n') {
+    alert('Ok, no offense taken! ;)');
+} else {
+    alert('Please answer yes/y or no/n.');
+}
 
 /* var willPlay = prompt('Should the guessing game be about me?'); 
     // If not, too bad. Answer again, and this time answer yes
     // otherwise my questions are done.
 */
+
+var ageGuess = prompt('How old do you think I am?');
+console.log('User guessed I am this old:', ageGuess);
+if (parseInt(ageGuess) === 'yes') {
+
+}
+alert('');
+
+// Use arrays in one or more questions

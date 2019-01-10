b90528302c056f23fb0edf3a9f1355a558dbd8c6
Leo Juarez <juarezleo9@gmail.com>
2019-01-10 15:52:58 -0800

    adds functions to final quetions

--- js/app.js
+++ js/app.js
@@ -43,116 +43,115 @@ function question1() {
 }
 
  question1();
-
-function question2() {
     
+// Y/N Preliminary Question 
+console.log('Y/N Question #2');
+var willPlay = prompt('Do ya want to try to guess my age?' +
+' Answer "yes" or "no."' +
+' (The answers "y" or "n" are also acceptable.)' );
+console.log('User wants to play:', willPlay);
+if (willPlay.toLowerCase() === 'yes' || willPlay.toLowerCase() === 'y') {
+    alert('Ok, let\'s play!');
+} else if (willPlay.toLowerCase() === 'no' || willPlay.toLowerCase() === 'n') {
+    alert('Ok, no offense taken! ;)');
+} else {
+    alert('Sorry, the answer is not valid. Please answer yes/y or no/n.');
+}
+
+if (willPlay.toLowerCase() === 'yes' || willPlay.toLowerCase() === 'y') { 
+// ^ Is there a more concise way to check if user has answered yes?
+    
+
     // Y/N Question #2
     console.log('Y/N Question #2');
-    var willPlay = prompt('Do ya want to try to guess my age?' +
-    ' Answer "yes" or "no."' +
-    ' (The answers "y" or "n" are also acceptable.)' );
-    console.log('User wants to play:', willPlay);
-    if (willPlay.toLowerCase() === 'yes' || willPlay.toLowerCase() === 'y') {
-        alert('Ok, let\'s play!');
-    } else if (willPlay.toLowerCase() === 'no' || willPlay.toLowerCase() === 'n') {
-        alert('Ok, no offense taken! ;)');
-    } else {
-        alert('Sorry, the answer is not valid. Please answer yes/y or no/n.');
-    }
-
-    if (willPlay.toLowerCase() === 'yes' || willPlay.toLowerCase() === 'y') { 
-        // ^ Is there a more concise way to check if user has answered yes?
-    }
-}
-question2();
-
-function question3() {
-
-    // Y/N Question #3
-    console.log('Y/N Question #3');
-    var youngerThanTwenty = prompt('Do you think I\'m younger than 20?' +
-    ' Answer yes/y or no/n.');
-    console.log('User\'s guess whether I\'m younger than 20:', youngerThanTwenty);
-    if (youngerThanTwenty.toLowerCase() === 'yes' || youngerThanTwenty.toLowerCase() === 'y') {
-        alert('I\'m older. Try again.');
-        console.log('User guessed yes, younger than 20:', youngerThanTwenty);
-    } else if (youngerThanTwenty.toLowerCase() === 'no' || youngerThanTwenty.toLowerCase() === 'n') {
-        alert('You\'re right, I\'m older. Here is another question.');
-        console.log('User guessed no, older than 20:', youngerThanTwenty);
-        score++;
-        console.log('Score is currently:', score);
-    } else {
-        alert('Please answer yes/y or no/n. Try another question.'); // Gotta handle invalid user input better
-        console.log('User\'s answer was invalid:', youngerThanTwenty);
-    }
-}
-
-question3();
-
-function question4() {
-
-    // Y/N Question #4
-    console.log('Y/N Question #4');
-    var youngerThanTwentyFive = prompt('Do you think I\'m younger than 25?' +
-    ' Answer yes/y or no/n.');
-    if (youngerThanTwentyFive.toLowerCase() === 'yes' || youngerThanTwentyFive.toLowerCase() === 'y') {
-        alert('I\'m actually older. Try again.');
-        console.log('User guessed yes, younger than 25:', youngerThanTwentyFive);
-    } else if (youngerThanTwentyFive.toLowerCase() === 'no' || youngerThanTwentyFive.toLowerCase() === 'n') {
-        alert('You\'re right, I\'m actually older. Here is another question.');
-        console.log('User guessed no, older than 25:', youngerThanTwentyFive);
-        score++;
-        console.log('Score is currently:', score);
-    } else {
-        alert('Please answer yes/y or no/n. Here\'s another question.'); // Gotta handle invalid user input better
-        console.log('User\'s answer was invalid:', youngerThanTwentyFive);
-    }
-}
-
-question4();
-
-function question5(){
-
-    // Y/N Question #5
-    console.log('Y/N Question #5');
-    var youngerThanThirty = prompt('Think I\'m younger than 30?' + 
-    ' Answer yes/y or no/n.');
-    if (youngerThanThirty.toLowerCase() === 'no' || youngerThanThirty.toLowerCase() === 'n') {
-        alert('Actually, I\'m not quite that old!');
-        console.log('User guessed no, not younger than 30:', youngerThanThirty);
-    } else if (youngerThanThirty.toLowerCase() === 'yes' || youngerThanThirty.toLowerCase() === 'y') {
-            alert('You\'re right, I\'m younger.');
-            console.log('User guessed yes, younger than 30:', youngerThanThirty);
+    function question2() {
+        var youngerThanTwenty = prompt('Do you think I\'m younger than 20?' +
+        ' Answer yes/y or no/n.');
+        console.log('User\'s guess whether I\'m younger than 20:', youngerThanTwenty);
+        if (youngerThanTwenty.toLowerCase() === 'yes' || youngerThanTwenty.toLowerCase() === 'y') {
+            alert('I\'m older. Try again.');
+            console.log('User guessed yes, younger than 20:', youngerThanTwenty);
+        } else if (youngerThanTwenty.toLowerCase() === 'no' || youngerThanTwenty.toLowerCase() === 'n') {
+            alert('You\'re right, I\'m older. Here is another question.');
+            console.log('User guessed no, older than 20:', youngerThanTwenty);
             score++;
             console.log('Score is currently:', score);
-    } else {
-        alert('Please answer yes/y or no/n. One last question.'); // Gotta handle invalid user input better
-        console.log('User\'s answer was invalid:', youngerThanThirty);
+        } else {
+            alert('Please answer yes/y or no/n. Try another question.'); // Gotta handle invalid user input better
+            console.log('User\'s answer was invalid:', youngerThanTwenty);
+        }
     }
 
+    question2();
+
+    function question3() {
+
+        // Y/N Question #3
+        console.log('Y/N Question #3');
+        var youngerThanTwentyFive = prompt('Do you think I\'m younger than 25?' +
+        ' Answer yes/y or no/n.');
+        if (youngerThanTwentyFive.toLowerCase() === 'yes' || youngerThanTwentyFive.toLowerCase() === 'y') {
+            alert('I\'m actually older. Try again.');
+            console.log('User guessed yes, younger than 25:', youngerThanTwentyFive);
+        } else if (youngerThanTwentyFive.toLowerCase() === 'no' || youngerThanTwentyFive.toLowerCase() === 'n') {
+            alert('You\'re right, I\'m actually older. Here is another question.');
+            console.log('User guessed no, older than 25:', youngerThanTwentyFive);
+            score++;
+            console.log('Score is currently:', score);
+        } else {
+            alert('Please answer yes/y or no/n. Here\'s another question.'); // Gotta handle invalid user input better
+            console.log('User\'s answer was invalid:', youngerThanTwentyFive);
+        }
+    }
+
+    question3();
+
+    function question4(){
+
+        // Y/N Question #4
+        console.log('Y/N Question #4');
+        var youngerThanThirty = prompt('Think I\'m younger than 30?' + 
+        ' Answer yes/y or no/n.');
+        if (youngerThanThirty.toLowerCase() === 'no' || youngerThanThirty.toLowerCase() === 'n') {
+            alert('Actually, I\'m not quite that old!');
+            console.log('User guessed no, not younger than 30:', youngerThanThirty);
+        } else if (youngerThanThirty.toLowerCase() === 'yes' || youngerThanThirty.toLowerCase() === 'y') {
+                alert('You\'re right, I\'m younger.');
+                console.log('User guessed yes, younger than 30:', youngerThanThirty);
+                score++;
+                console.log('Score is currently:', score);
+        } else {
+            alert('Please answer yes/y or no/n. One last question.'); // Gotta handle invalid user input better
+            console.log('User\'s answer was invalid:', youngerThanThirty);
+        }
+
+    }
+
+    question4();
+
+    //  Y/N question #5
+    console.log('Y/N question #5');
+    function question5() {    
+
+        var ageOptions = [26, 27, 28];
+        var multipleChoiceGuess = prompt('Think my age is one of the following? ' +
+        ageOptions[0].toString() + ', ' + ageOptions[1].toString() + ', ' +
+        ageOptions[2].toString() + ' (Answer yes/y or no/n.)');
+        if (multipleChoiceGuess.toLowerCase() === 'yes' || multipleChoiceGuess.toLowerCase() === 'y') {
+            alert('Nope! I\'m actually ' + myAge);
+        } else if (multipleChoiceGuess.toLowerCase() === 'no' || multipleChoiceGuess.toLowerCase() === 'n') {
+            alert('Correct, I\'m ' + myAge + '!');
+            score++;
+            console.log('Score is currently:', score);
+        } else {
+            alert('Sorry, invalid answer.');
+        }
+    }
+    question5();
+
+        alert('Thanks for playing!');
+        console.log('Game over!');
 }
-
-question5();
-
-    // A bonus Y/N question
-    console.log('Bonus Y/N question');
-    var ageOptions = [26, 27, 28];
-    var multipleChoiceGuess = prompt('Think my age is one of the following? ' +
-    ageOptions[0].toString() + ', ' + ageOptions[1].toString() + ', ' +
-    ageOptions[2].toString() + ' (Answer yes/y or no/n.)');
-    if (multipleChoiceGuess.toLowerCase() === 'yes' || multipleChoiceGuess.toLowerCase() === 'y') {
-        alert('Nope! I\'m actually ' + myAge);
-    } else if (multipleChoiceGuess.toLowerCase() === 'no' || multipleChoiceGuess.toLowerCase() === 'n') {
-        alert('Correct, I\'m ' + myAge + '!');
-        score++;
-        console.log('Score is currently:', score);
-    } else {
-        alert('Sorry, invalid answer.');
-    }
-
-    alert('Thanks for playing!');
-    console.log('Game over!');
-
 
 
 // Question #6
@@ -231,12 +230,13 @@ function question7() {
             rightAnswers[0] + ', ' + rightAnswers[1] + ', and ' + rightAnswers[2]);
             // guessesRemaining = 0;
             break;
-            }
+        }
     }
 }
 question7();
     // console.log('Confirm final username:', userName);
     console.log('User\'s score was:', score);
+
 
     // User won't be able to score all possible
     // points if they opt out of guessing my age.
